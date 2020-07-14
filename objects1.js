a69f6e74809eb72ba21486dd920a6d4ed5898ad0
Elias <eli.bueno9@gmail.com>
2020-07-14 09:51:19 -0300

    Challenge 1-3 part 1 done

---
+++ objects1.js
@@ -0,0 +1,59 @@
+//Create a program which picks data from an object
+const programmers = [
+    {
+        name: 'John',
+        age: '40',
+        tecnologies: [
+            {name: 'JavaScript', specialty: 'Web/Mobile'},
+            {name: 'Python', specialty: 'Desktop'},
+            {name: 'Swift', specialty: 'IOS'},
+            {name: 'React', specialty: 'Web'}
+        ]
+    },
+    {
+        name: 'Sue',
+        age: '22',
+        tecnologies: [
+            {name: 'Java', specialty: 'Desktop'},
+            {name: 'Python', specialty: 'Desktop'},
+            {name: 'Kotlin', specialty: 'Mobile'},
+            {name: 'Dart', specialty: 'Desktop'}
+        ]
+    },
+    {
+        name: 'Suellen',
+        age: '20',
+        tecnologies: [
+            {name: 'PHP', specialty: 'Web Server'},
+            {name: 'Go', specialty: 'Desktop'},
+            {name: 'Kotlin', specialty: 'Mobile'}
+        ]
+    }
+] 
+
+
+identifyTecnologies = (anObject) => {
+    let tecnologiesLength = anObject.tecnologies.length;
+    let tecnologies = "";
+        for (let j = 0; j < tecnologiesLength; j++) {
+            if(j !== tecnologiesLength - 1)
+                tecnologies += anObject.tecnologies[j].name + ` (${anObject.tecnologies[j].specialty})` + ", ";
+            else
+                tecnologies += anObject.tecnologies[j].name + ` (${anObject.tecnologies[j].specialty})`; 
+        }
+    console.log(tecnologies);
+}
+
+
+
+//For each programmer, use identifyTecnologies
+identifyProgrammers = (anArray) => {
+    let anArrayLength = anArray.length;
+    for(let i = 0; i < anArrayLength; i++) {
+        console.log(`${anArray[i].name} knows the following tecnologies: `);
+        identifyTecnologies(anArray[i]);
+    }
+}
+
+//Execution
+identifyProgrammers(programmers);
