b7bb059f8e5f4eb88ccd6a46eed4f3c2f36aeeb6
Elias <eli.bueno9@gmail.com>
2020-07-22 09:31:11 -0300

    Function getHigherTransactionsByType() done!

--- bankOperations/bankOperations.js
+++ bankOperations/bankOperations.js
@@ -7,15 +7,23 @@ const user = {
 const newTransactions = [
     {
         type: 'credit',
-        value: 50.5
-    },
-    {
-        type: 'debit',
-        value: 100
+        value: 50
     },
     {
         type: 'credit',
-        value: 50
+        value: 120
+    },
+    {
+        type: 'credit',
+        value: 120
+    },
+    {
+        type: 'debit',
+        value: 80
+    },
+    {
+        type: 'debit',
+        value: 30
     }
 ]
 
@@ -33,5 +41,29 @@ function addAllNewTransactions (transactions, aUser) {
     }
 }
 
+//Add the transactions
 addAllNewTransactions(newTransactions, user)
-console.log(user);
+
+//See the new User Data
+//console.log(user);
+
+// Now let's identify the transactions whose values are the highest
+function getHigherTransactionByType (aUser, type) {
+    let numberOfTransactions = aUser.transactions.length;
+    let higherCreditValue = -Infinity, higherDebitValue = -Infinity ;
+    for(let i = 0; i < numberOfTransactions; i++) {
+        if(aUser.transactions[i].type === "credit" && aUser.transactions[i].value >= higherCreditValue)
+            higherCreditValue = aUser.transactions[i].value;
+        else {
+            if(aUser.transactions[i].value > higherDebitValue)
+                higherDebitValue = aUser.transactions[i].value;
+        }
+    }
+    if(type === "credit")
+        return { type: "credit", value: higherCreditValue };
+    else
+        return { type: "debit", value: higherDebitValue };
+}
+
+//Testing
+console.log(getHigherTransactionByType(user, "credit"))
