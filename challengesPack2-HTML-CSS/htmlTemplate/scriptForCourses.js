62659e9f4beae5f445fcb2386f7507b95fc2ff2c
Elias <eli.bueno9@gmail.com>
2020-08-17 10:46:23 -0300

    More styling and controlling modal with JavaScript

--- challengesPack2-HTML-CSS/htmlTemplate/scriptForCourses.js
+++ challengesPack2-HTML-CSS/htmlTemplate/scriptForCourses.js
@@ -4,9 +4,20 @@ const modalOverlay = document.querySelector(".modalOverlay");
 // Add the close modal element in variable
 const closingModal = modalOverlay.querySelector("#closeModal");
 
+// Add the cards in a variable
+const courses = document.querySelectorAll(".course");
+
+// For each one of the courses, add an event listener which removes the notDisplay class to the modalOverlay
+for(let course of courses) {
+    course.addEventListener("click",
+        () => {
+            modalOverlay.classList.remove("notDisplay")
+        }    
+    )
+}
 
 
-// Add an event listener to it
+// Add an event listener to it to add the modal overlay
 closingModal.addEventListener("click", 
     () => {
         modalOverlay.classList.add("notDisplay");
