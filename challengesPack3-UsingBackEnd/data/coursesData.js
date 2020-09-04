b4c5716c8ed1808ab38bd10ae87812fe607b7c59
Elias <eli.bueno9@gmail.com>
2020-09-04 08:40:14 -0300

    Data ready

---
+++ challengesPack3-UsingBackEnd/data/coursesData.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        courseId = "starter",
+        imgSrc = "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/starter.svg",
+        altText = "Starter course logo",
+        descriptionTitle = "A start in your career with the free courses",
+        courseInformation: {
+            modules: "7",
+            spanClass = "free",
+            price: "Free"
+        }
+    },
+    {
+        courseId = "launchbase",
+        imgSrc = "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/launchbase.svg",
+        altText = "LaunchBase course Logo",
+        descriptionTitle = "Become a great programmer with the fundamental web concepts and technologies",
+        courseInformation: {
+            modules: "8",
+            spanClass = "paid",
+            price: "Paid"
+        }
+    },
+    {
+        courseId = "gostack",
+        imgSrc = "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/gostack.svg",
+        altText = "GoStack course Logo",
+        descriptionTitle = "Be part of the best developers by learning the most powerful techonologies of the Web",
+        courseInformation: {
+            modules: "7",
+            spanClass = "paid",
+            price: "Paid"
+        }
+    }
+];
