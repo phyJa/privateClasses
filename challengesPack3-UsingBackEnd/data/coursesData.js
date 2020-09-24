b2d6b5cfa465f2fa01643f4c5618063003df76ca
Elias <eli.bueno9@gmail.com>
2020-09-24 09:46:52 -0300

    Rewrinting 'description' in the courses data

--- challengesPack3-UsingBackEnd/data/coursesData.js
+++ challengesPack3-UsingBackEnd/data/coursesData.js
@@ -4,7 +4,7 @@ module.exports = [
         imgSrc: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/starter.svg",
         altText: "Starter course logo",
         descriptionTitle: "A start in your career with the free courses",
-        descritpion: ` In this course, you will learn a lot about everything you need to get
+        description: ` In this course, you will learn a lot about everything you need to get
         started with the universe of programming. `,
         specifications: {
             modules: "7",
@@ -17,7 +17,7 @@ module.exports = [
         imgSrc: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/launchbase.svg",
         altText: "LaunchBase course Logo",
         descriptionTitle: "Become a great programmer with the fundamental web concepts and technologies",
-        descritpion: ` Here, you will learn more solid concepts and techniques you need to get
+        description: ` Here, you will learn more solid concepts and techniques you need to get
         your first programming job. `,
         specifications: {
             modules: "8",
@@ -30,7 +30,7 @@ module.exports = [
         imgSrc: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/gostack.svg",
         altText: "GoStack course Logo",
         descriptionTitle: "Be part of the best developers by learning the most powerful techonologies of the Web",
-        descritpion: ` With the GoStack course, get ready to be desired by big companies! Here you will learn 
+        description: ` With the GoStack course, get ready to be desired by big companies! Here you will learn 
         more advanced techniques, concepts and tools, such as ReactJS and React Native, to put your application
         into the next level! `,
         specifications: {
