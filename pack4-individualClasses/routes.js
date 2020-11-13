eefcfd260bb3347b2d2d7a0b962c0f970356447e
Elias <eli.bueno9@gmail.com>
2020-11-13 10:15:44 -0300

    Writing first code in create file

--- pack4-individualClasses/routes.js
+++ pack4-individualClasses/routes.js
@@ -2,12 +2,6 @@ const express = require("express");
 
 const routes = express.Router();
 
-routes.get(
-
-    "/",
-
-    (request, response) => { return response.render("landing"); }
-
-);
+routes.get("/", (request, response) => { return response.render("landing"); });
 
 module.exports = routes;
