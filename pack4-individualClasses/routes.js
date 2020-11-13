212dcb064b8d93388f4cc316e83ff84010cea1a6
Elias <eli.bueno9@gmail.com>
2020-11-13 10:30:13 -0300

    Adding a route to create a techer

--- pack4-individualClasses/routes.js
+++ pack4-individualClasses/routes.js
@@ -2,6 +2,17 @@ const express = require("express");
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => { return response.render("landing"); });
+routes.get(
+    "/",
+    function (request, response) {
+        return response.render("landing"); 
+    }
+);
+routes.get(
+    "/teachers/create", 
+    function (request, response) {
+        return response.render("teachers/create");
+    }
+);
 
 module.exports = routes;
