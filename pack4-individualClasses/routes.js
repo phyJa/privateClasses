5e0572608a095078b5f061fc9868c376ca0673bb
Elias <eli.bueno9@gmail.com>
2020-11-16 09:06:00 -0300

    Removing extra line spaces

--- pack4-individualClasses/routes.js
+++ pack4-individualClasses/routes.js
@@ -1,9 +1,10 @@
+// Call express and initiate the router
 const express = require("express");
 const routes = express.Router();
 // Functions to render pages
 const renderPages = require("./functions/renderPages");
-
+// Routes
 routes.get("/", renderPages.renderLanding);
 routes.get("/teachers/create", renderPages.renderCreate);
-
+// Export
 module.exports = routes;
