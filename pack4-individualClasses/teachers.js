b8032ec47e12fb0567134c261cb4a071e5b7a62d
Elias <eli.bueno9@gmail.com>
2020-11-17 10:11:20 -0300

    Validation done

--- pack4-individualClasses/teachers.js
+++ pack4-individualClasses/teachers.js
@@ -1,3 +1,15 @@
+// Requires
+const fs = require("fs");
+// Exports
 exports.validateAndWriteData = function(request, response) {
-    return response.send(request.body);
+    // Validation
+    const dataKeys = Object.keys(request.body);
+    const data = request.body;
+    for(let aDataKey of dataKeys) {
+        if(data[aDataKey] === "")
+            return response.status(400).send("Please, fill all the fields");
+    }
+    // Write
+    // If everything goes fine
+    return response.send(dataKeys);
 }
