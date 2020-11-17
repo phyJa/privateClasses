2c819ff04509e65a42d44445b184fd5822e9702a
Elias <eli.bueno9@gmail.com>
2020-11-17 09:11:12 -0300

    Removing extra lines

--- pack4-individualClasses/server.js
+++ pack4-individualClasses/server.js
@@ -7,29 +7,18 @@ const server = express();
 const routes = require("./routes");
 
 // Settings
-
 server.set("view engine", "njk");
-
 server.use(express.static("public"));
 // Enable POST visualization
 server.use(express.urlencoded({extended: true}));
-
 nunjucks.configure(
-
     "pages",
-
     {
-
         express: server,
-
         autoescape: false, // This is for nunjucks to recognize internal html tags in variables
-
         noCache: true // Not use cache (a version which is saved in the computer).
-
     }
-
 );
-
 server.use(routes);
-
+// Listen
 server.listen(4000, () => { console.log("Listening!" ); } );
